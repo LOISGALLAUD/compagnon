df046ed4d937079e44a415acf198fa0947205662
LOISGALLAUD <loisgallaud@hotmail.com>
2024-02-17 16:14:20 +0100

    hero responsive

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -6,10 +6,10 @@ const HeroCard = () => {
       <div className="2xl:w-7/12 w-full">
         {/* Text */}
         <div className="flex flex-col font-jeko gap-5 text-pond lg:text-start text-center">
-          <h2 className="lg:text-7xl text-3xl">
+          <h2 className="lg:text-7xl sm:text-5xl text-2xl">
             Book now your
             <br />
-            <h1 className="font-mhoko lg:text-9xl md:text-6xl text-4xl text-abyss py-3">
+            <h1 className="font-mhoko lg:text-9xl sm:text-6xl text-4xl text-abyss py-3">
               {" "}
               Compagnon{" "}
             </h1>
@@ -22,16 +22,16 @@ const HeroCard = () => {
         </div>
 
         {/* Buttons */}
-        <div className="flex flex-row gap-5 pt-12">
-          <button className="bg-pond text-white font-jeko text-2xl py-3 rounded-lg p-3">
+        <div className="flex flex-row lg:justify-start justify-center lg:gap-5 gap-2 lg:pt-12 pt-6">
+          <button className="bg-pond text-white text-center font-jekom lg:text-2xl sm:text-lg text-xs lg:py-3 py-1 rounded-lg p-3">
             Book a visit
           </button>
-          <button className="bg-abyss text-white font-jeko text-2xl py-3 rounded-lg p-3">
+          <button className="bg-abyss text-white text-center font-jekom lg:text-2xl sm:text-lg text-xs lg:py-3 py-1 rounded-lg p-3">
             Learn more about us
             <img
               src="/images/broche.png"
               alt="logo"
-              className="w-[3rem] h-[3rem] inline-block object-cover ml-3"
+              className="lg:w-[3rem] lg:h-[3rem] w-[2rem] h-[2rem] lg:inline-block object-cover ml-3 hidden"
             />
           </button>
         </div>
